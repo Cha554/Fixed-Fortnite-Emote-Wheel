c117b5f12ff1a49d80d5398338f4fdb6d7314292
Cha <113856355+Cha554@users.noreply.github.com>
2026-03-09 01:09:30 -0400

    whole thing

---
+++ PluginInfo.cs
@@ -0,0 +1,9 @@
+﻿namespace FortniteEmoteWheel
+{
+    internal class PluginInfo
+    {
+        public const string GUID = "com.goldentrophy.gorillatag.fortniteemotewheel";
+        public const string Name = "FortniteEmoteWheel";
+        public const string Version = "1.4.0";
+    }
+}
